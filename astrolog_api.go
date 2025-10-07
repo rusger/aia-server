0d1f97b8262eb6df05ba47549d4774d5e4edad29
RuslanGerasimov3 <somasuryagnilocana@gmail.com>
2025-10-07 20:29:47 +0200

    "-c", "14", "-C", "-RC", "22", "31"

--- astrolog_api.go
+++ astrolog_api.go
@@ -278,7 +278,7 @@ func calculateChart(w http.ResponseWriter, r *http.Request) {
         latitude,
         "-s", "0.883208",
         "-R", "8", "9", "10",
-        "-c", "14", "-C", "-RC", "22", "31"
+        "-c", "14", "-C", "-RC", "22", "31",
     }
 
     // Execute command with timeout
